293176c0b855c457d86068d1457f8c20051ee7b1
Nathaniel Varona <nathanielvarona@users.noreply.github.com>
2017-07-02 05:04:44 +0800

    [AIRFLOW-1368] Automatically remove the container when it exits

--- airflow/operators/docker_operator.py
+++ airflow/operators/docker_operator.py
@@ -18,7 +18,7 @@ from airflow.exceptions import AirflowException
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 from airflow.utils.file import TemporaryDirectory
-from docker import Client, tls
+from docker import APIClient, tls
 import ast
 
 
@@ -75,6 +75,8 @@ class DockerOperator(BaseOperator):
     :type xcom_push: bool
     :param xcom_all: Push all the stdout or just the last line. The default is False (last line).
     :type xcom_all: bool
+    :param auto_remove: Automatically remove the container when it exits
+    :type auto_remove: bool
     """
     template_fields = ('command',)
     template_ext = ('.sh', '.bash',)
@@ -101,6 +103,7 @@ class DockerOperator(BaseOperator):
             volumes=None,
             xcom_push=False,
             xcom_all=False,
+            auto_remove=False,
             *args,
             **kwargs):
 
@@ -124,6 +127,7 @@ class DockerOperator(BaseOperator):
         self.volumes = volumes or []
         self.xcom_push_flag = xcom_push
         self.xcom_all = xcom_all
+        self.auto_remove = auto_remove
 
         self.cli = None
         self.container = None
@@ -142,7 +146,7 @@ class DockerOperator(BaseOperator):
             )
             self.docker_url = self.docker_url.replace('tcp://', 'https://')
 
-        self.cli = Client(base_url=self.docker_url, version=self.api_version, tls=tls_config)
+        self.cli = APIClient(base_url=self.docker_url, version=self.api_version, tls=tls_config)
 
         if ':' not in self.image:
             image = self.image + ':latest'
@@ -165,8 +169,10 @@ class DockerOperator(BaseOperator):
                     command=self.get_command(),
                     cpu_shares=cpu_shares,
                     environment=self.environment,
-                    host_config=self.cli.create_host_config(binds=self.volumes,
-                                                            network_mode=self.network_mode),
+                    host_config=self.cli.create_host_config(
+                                                binds=self.volumes,
+                                                network_mode=self.network_mode,
+                                                auto_remove=self.auto_remove),
                     image=image,
                     mem_limit=self.mem_limit,
                     user=self.user
